5024ec730f562e554450f9995b984746ffb92376
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-01-09 19:46:54 +0300

    add layers

--- app/src/main/java/com/example/moviesearch/presentation/MainActivity.kt
+++ app/src/main/java/com/example/moviesearch/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.moviesearch
+package com.example.moviesearch.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.moviesearch.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
