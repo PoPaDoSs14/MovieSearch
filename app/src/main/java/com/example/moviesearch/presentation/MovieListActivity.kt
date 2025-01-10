de8eceea5a526891077a343021919139d2384c28
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-01-10 21:35:55 +0300

    add MovieListActivity

---
+++ app/src/main/java/com/example/moviesearch/presentation/MovieListActivity.kt
@@ -0,0 +1,16 @@
+package com.example.moviesearch.presentation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.moviesearch.R
+
+class MovieListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_list)
+
+    }
+}
