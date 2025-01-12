526861ade765222ad4e7553d01459e54aa0cf3db
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-01-12 19:10:08 +0300

    add viewBinding in Activities

--- app/src/main/java/com/example/moviesearch/presentation/MainActivity.kt
+++ app/src/main/java/com/example/moviesearch/presentation/MainActivity.kt
@@ -6,13 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.moviesearch.R
+import com.example.moviesearch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
--- app/src/main/java/com/example/moviesearch/presentation/MovieListActivity.kt
+++ app/src/main/java/com/example/moviesearch/presentation/MovieListActivity.kt
@@ -6,11 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.moviesearch.R
+import com.example.moviesearch.databinding.ActivityMovieListBinding
 
 class MovieListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMovieListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie_list)
+        binding = ActivityMovieListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
