5825604556035ecb2b1dd0d85556bb84ef3fe64b
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-01-14 18:13:20 +0300

    add GsonConverterFactory

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ android {
 dependencies {
 
     implementation(libs.retrofit)
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.google.dagger:dagger:2.44")
     kapt ("com.google.dagger:dagger-compiler:2.44")
     implementation ("com.google.code.gson:gson:2.11.0")
