da1a681f2dd55a755a9f8980e45071399b3b359f
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-01-14 18:13:42 +0300

    add ApiFactory

--- app/src/main/java/com/example/moviesearch/data/ApiFactory.kt
+++ app/src/main/java/com/example/moviesearch/data/ApiFactory.kt
@@ -1,4 +1,19 @@
 package com.example.moviesearch.data
 
+import com.google.gson.Gson
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
 object ApiFactory {
+
+    private const val BASE_URL: String = "test"
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .build()
+
+    val api: Api = retrofit.create(Api::class.java)
+
 }
