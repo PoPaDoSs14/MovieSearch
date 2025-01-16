2c338b724f2dda45c13ac56e05dcc121759a1397
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-01-16 20:22:23 +0300

    add DomainModule

--- app/src/main/java/com/example/moviesearch/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/moviesearch/di/ApplicationComponent.kt
@@ -5,7 +5,7 @@ import com.example.moviesearch.presentation.MovieListViewModel
 import dagger.Component
 import dagger.Module
 
-@Component(modules = [DataModule::class])
+@Component(modules = [DataModule::class, DomainModule::class])
 interface ApplicationComponent {
 
 
---
+++ app/src/main/java/com/example/moviesearch/di/DomainModule.kt
@@ -0,0 +1,7 @@
+package com.example.moviesearch.di
+
+import dagger.Module
+
+@Module
+class DomainModule {
+}
