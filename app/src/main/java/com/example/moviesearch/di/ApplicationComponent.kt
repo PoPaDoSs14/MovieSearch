d2691362dd06a036de6232e0c30dc35d364048f9
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-01-16 20:21:06 +0300

    add di package

---
+++ app/src/main/java/com/example/moviesearch/di/ApplicationComponent.kt
@@ -0,0 +1,16 @@
+package com.example.moviesearch.di
+
+import com.example.moviesearch.presentation.FindMovieViewModel
+import com.example.moviesearch.presentation.MovieListViewModel
+import dagger.Component
+import dagger.Module
+
+@Component(modules = [DataModule::class])
+interface ApplicationComponent {
+
+
+    fun inject(viewModel: MovieListViewModel)
+
+    fun inject(viewModel: FindMovieViewModel)
+
+}
