f971e0d13cbc341b3b0c299e05a6872c86fd9b65
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-01-17 19:51:09 +0300

    add AppComponent

--- app/src/main/java/com/example/moviesearch/App.kt
+++ app/src/main/java/com/example/moviesearch/App.kt
@@ -1,6 +1,13 @@
 package com.example.moviesearch
 
 import android.app.Application
+import com.example.moviesearch.di.DaggerApplicationComponent
 
 class App(): Application() {
+
+    val component by lazy {
+        DaggerApplicationComponent
+            .factory()
+            .create()
+    }
 }
