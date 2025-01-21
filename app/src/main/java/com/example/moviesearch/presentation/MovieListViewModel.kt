97a5b8dcdcc4b64fba91cbe91b1ee86791840d30
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-01-21 20:07:55 +0300

    add movie

--- app/src/main/java/com/example/moviesearch/presentation/MovieListViewModel.kt
+++ app/src/main/java/com/example/moviesearch/presentation/MovieListViewModel.kt
@@ -2,14 +2,31 @@ package com.example.moviesearch.presentation
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.moviesearch.data.RepositoryImpl
+import com.example.moviesearch.domain.Movie
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class MovieListViewModel @Inject constructor(): ViewModel() {
+class MovieListViewModel @Inject constructor(
+    val repositoryImpl: RepositoryImpl
+): ViewModel() {
+
+    lateinit var movie: Movie
+
+    init {
+        getMovies()
+    }
 
 
     fun test(){
         Log.d("MovieListViewModel", "MovieListViewModel")
+    }
+
+    fun getMovies() {
+        viewModelScope.launch {
+            movie = repositoryImpl.getMovies()
+        }
     }
 
 }
