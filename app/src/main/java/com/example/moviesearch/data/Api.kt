17d7aaf7a6de856aa195cbe29a95aa7287a99107
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-01-23 20:03:19 +0300

    update Api

--- app/src/main/java/com/example/moviesearch/data/Api.kt
+++ app/src/main/java/com/example/moviesearch/data/Api.kt
@@ -3,10 +3,14 @@ package com.example.moviesearch.data
 import com.example.moviesearch.domain.Movie
 import kotlinx.coroutines.flow.Flow
 import retrofit2.http.GET
+import retrofit2.http.Query
 import javax.inject.Inject
 
 interface Api {
 
-    @GET("")
-    suspend fun getMovies(): Movie
+    @GET("?")
+    suspend fun getMovies(
+        @Query("s") search: String = "Avengers Endgame",
+        @Query("r") responseFormat: String = "json"
+    ): Movie
 }
