3c0303c088c23539aeb0521f354965322ff8fb70
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-01-30 17:34:36 +0300

    update Api to kinopoisk

--- app/src/main/java/com/example/moviesearch/data/Api.kt
+++ app/src/main/java/com/example/moviesearch/data/Api.kt
@@ -3,14 +3,15 @@ package com.example.moviesearch.data
 import com.example.moviesearch.domain.Movie
 import kotlinx.coroutines.flow.Flow
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Query
 import javax.inject.Inject
 
 interface Api {
-
-    @GET("?")
+    @GET("v1.4/movie")
     suspend fun getMovies(
-        @Query("s") search: String = "Avengers Endgame",
-        @Query("r") responseFormat: String = "json"
+        @Query("year") year: Int,
+        @Query("genres.name") genre: String,
+        @Header("X-API-KEY") apiKey: String
     ): Movie
 }
