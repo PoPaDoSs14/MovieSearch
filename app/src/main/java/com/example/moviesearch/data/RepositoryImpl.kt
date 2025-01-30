cfa6a4f85175f720f410635770566340b28b7585
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-01-30 17:51:01 +0300

    update data lay

--- app/src/main/java/com/example/moviesearch/data/RepositoryImpl.kt
+++ app/src/main/java/com/example/moviesearch/data/RepositoryImpl.kt
@@ -2,14 +2,11 @@ package com.example.moviesearch.data
 
 import com.example.moviesearch.domain.Movie
 import com.example.moviesearch.domain.Repository
-import com.example.moviesearch.domain.Search
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.toList
 import javax.inject.Inject
 
 class RepositoryImpl @Inject constructor(
     val api: ApiFactory
 ): Repository {
 
-    override suspend fun getMovies(search: String): Movie = api.api.getMovies(search = search)
+    override suspend fun getMovies(search: String): Movie = api.api.getMovies(name = search, TODO())
 }
