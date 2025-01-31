52cd9ca61e920e2cf6091f9daa04cf64d703835d
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-01-31 21:05:01 +0300

    add provideApiKey in DataModule

--- app/src/main/java/com/example/moviesearch/di/DataModule.kt
+++ app/src/main/java/com/example/moviesearch/di/DataModule.kt
@@ -3,12 +3,20 @@ package com.example.moviesearch.di
 import com.example.moviesearch.data.ApiFactory
 import dagger.Module
 import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 class DataModule {
 
     @Provides
-    fun provideApiFactory(): ApiFactory{
-        return ApiFactory()
+    @Singleton
+    fun provideApiKey(): String {
+        return "api-test"
+    }
+
+    @Provides
+    @Singleton
+    fun provideApiFactory(apiKey: String): ApiFactory {
+        return ApiFactory(apiKey)
     }
 }
