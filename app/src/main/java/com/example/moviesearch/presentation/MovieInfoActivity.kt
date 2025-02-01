98f01d626641317d61d9f40b874943706b5e860b
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-02-01 21:01:33 +0300

    add binding in MovieInfoActivity

--- app/src/main/java/com/example/moviesearch/presentation/MovieInfoActivity.kt
+++ app/src/main/java/com/example/moviesearch/presentation/MovieInfoActivity.kt
@@ -6,10 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.moviesearch.R
+import com.example.moviesearch.databinding.ActivityMovieInfoBinding
 
 class MovieInfoActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMovieInfoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie_info)
+        binding = ActivityMovieInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
