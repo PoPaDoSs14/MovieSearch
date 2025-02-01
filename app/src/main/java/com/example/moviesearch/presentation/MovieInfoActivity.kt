1f1de881679895d2d226ab4b8781055a3d20ed5b
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-02-01 20:59:06 +0300

    add MovieInfoActivity

---
+++ app/src/main/java/com/example/moviesearch/presentation/MovieInfoActivity.kt
@@ -0,0 +1,15 @@
+package com.example.moviesearch.presentation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.moviesearch.R
+
+class MovieInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_info)
+    }
+}
