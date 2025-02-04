3a5b8ea93f60bd386df79c7627e713c1262a7aba
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-02-04 18:54:01 +0300

    add new inject method

--- app/src/main/java/com/example/moviesearch/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/moviesearch/di/ApplicationComponent.kt
@@ -2,6 +2,7 @@ package com.example.moviesearch.di
 
 import com.example.moviesearch.presentation.FindMovieActivity
 import com.example.moviesearch.presentation.FindMovieViewModel
+import com.example.moviesearch.presentation.MovieInfoActivity
 import com.example.moviesearch.presentation.MovieListActivity
 import com.example.moviesearch.presentation.MovieListViewModel
 import dagger.Component
@@ -14,6 +15,7 @@ interface ApplicationComponent {
     // Методы для внедрения зависимостей в Activity
     fun inject(activity: FindMovieActivity)
     fun inject(activity: MovieListActivity)
+    fun inject(activity: MovieInfoActivity)
 
     // Другие инъекции, если необходимы
 
