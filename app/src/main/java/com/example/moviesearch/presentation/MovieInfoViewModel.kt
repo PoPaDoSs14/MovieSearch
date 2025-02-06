f6151f7ecf978b36286a12dac63fc6613ac703a7
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-02-06 20:24:29 +0300

    add getMovies

--- app/src/main/java/com/example/moviesearch/presentation/MovieInfoViewModel.kt
+++ app/src/main/java/com/example/moviesearch/presentation/MovieInfoViewModel.kt
@@ -1,10 +1,23 @@
 package com.example.moviesearch.presentation
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.moviesearch.data.RepositoryImpl
+import com.example.moviesearch.domain.Movie
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class MovieInfoViewModel @Inject constructor(
     val repo: RepositoryImpl
 ): ViewModel() {
+
+
+    fun getMovies(search: String): Movie {
+        var movies: Movie? = null
+        viewModelScope.launch(Dispatchers.IO){
+            movies = repo.getMovies(search)
+        }
+        return movies?: throw RuntimeException("movies == null")
+    }
 }
