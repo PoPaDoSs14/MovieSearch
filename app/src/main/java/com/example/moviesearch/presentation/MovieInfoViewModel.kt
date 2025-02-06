9c28dcacaf8358a43348046502bc414cb589f18b
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-02-06 20:23:40 +0300

    add repo in constructor

--- app/src/main/java/com/example/moviesearch/presentation/MovieInfoViewModel.kt
+++ app/src/main/java/com/example/moviesearch/presentation/MovieInfoViewModel.kt
@@ -1,7 +1,10 @@
 package com.example.moviesearch.presentation
 
 import androidx.lifecycle.ViewModel
+import com.example.moviesearch.data.RepositoryImpl
 import javax.inject.Inject
 
-class MovieInfoViewModel @Inject constructor(): ViewModel() {
+class MovieInfoViewModel @Inject constructor(
+    val repo: RepositoryImpl
+): ViewModel() {
 }
